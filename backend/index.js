0e876538a46a781d8da46b23ada58918a3cf9ebc
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-09-29 21:50:27 -0400

    init backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+
+const app = express()
+const port = 4000
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Server runnign in port ${port}`)
+})
+
