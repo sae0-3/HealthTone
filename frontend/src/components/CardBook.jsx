7b95c496baef8b92aba0b56d354ccf822ede9430
Jhojan Rios <202202883@est.umss.edu>
2024-10-02 23:56:33 -0400

    pagina de contenido del libro actual

--- frontend/src/components/CardBook.jsx
+++ frontend/src/components/CardBook.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../styles/card.css';
+import med from '../icons/med.jpg'
 
 export const CardBook = () => {
   return (
     <div className='card-container'>
       <div className='card-poster'>
-        <img src="../hola" alt="img"/>
+        <img src={med} alt="img"/>
       </div>
-      <div className='card-content'>
+      <div className='card-text'>
         <h2>Titulo</h2>
         <p>Autor</p>
       </div>
--- frontend/src/components/ContentBook.jsx
+++ frontend/src/components/ContentBook.jsx
@@ -6,9 +6,23 @@ export const ContentBook = () => {
     return (
         <>
             <div className='content-container'>
-                <CardBook></CardBook>
+                <div className='card-container'>
+                    <CardBook></CardBook>
+                </div>
+                <div className='text-container'>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis libero odit, soluta dolor voluptatum, pariatur necessitatibus ad incidunt alias ullam ipsam ut, earum dolores voluptatem vitae a aperiam. Neque, vitae?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque excepturi totam eum culpa in minima, veniam doloremque inventore laborum illo. Non ipsa similique quaerat sed molestias enim impedit dicta earum?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, consequuntur. Ducimus, incidunt? Aspernatur fugit veritatis quis? Voluptatibus natus amet accusamus quis odio, modi voluptatem perferendis earum expedita vitae ex enim.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eos voluptatibus ipsum sint impedit voluptates expedita numquam fugiat ullam, doloremque ex eius autem nesciunt dolorem voluptas dignissimos adipisci accusantium hic.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis libero odit, soluta dolor voluptatum, pariatur necessitatibus ad incidunt alias ullam ipsam ut, earum dolores voluptatem vitae a aperiam. Neque, vitae?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque excepturi totam eum culpa in minima, veniam doloremque inventore laborum illo. Non ipsa similique quaerat sed molestias enim impedit dicta earum?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, consequuntur. Ducimus, incidunt? Aspernatur fugit veritatis quis? Voluptatibus natus amet accusamus quis odio, modi voluptatem perferendis earum expedita vitae ex enim.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eos voluptatibus ipsum sint impedit voluptates expedita numquam fugiat ullam, doloremque ex eius autem nesciunt dolorem voluptas dignissimos adipisci accusantium hic.
+                    </p>
+                </div>
             </div>
-            <p></p>
-    </>
+        </>
     )
 }
+
