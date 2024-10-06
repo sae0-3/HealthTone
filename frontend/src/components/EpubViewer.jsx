688352397bdc361771bf6322b1e1dde0d880a360
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-06 10:28:08 -0400

    propuesta de audio

--- frontend/src/components/EpubViewer.jsx
+++ frontend/src/components/EpubViewer.jsx
@@ -1,22 +1,56 @@
-import React, { useState } from 'react'
+import { useState, useRef } from 'react'
 import { ReactReader } from 'react-reader'
 
 
 export const EpubViewer = ({ url }) => {
   const [location, setLocation] = useState(null)
-  
+  const renditionRef = useRef(null)
+
   const handleLocationChanged = (newLocation) => {
     setLocation(newLocation)
-    console.log('Location changed to:', newLocation)
+  }
+
+  const narrateCurrentPage = () => {
+    if (renditionRef.current) {
+      const iframe = renditionRef.current.getContents()[0].document
+      const text = iframe.body.innerText
+      speak(text)
+    }
+  }
+
+  const speak = (text) => {
+    if (window.speechSynthesis) {
+      const utterance = new SpeechSynthesisUtterance(text)
+      speechSynthesis.speak(utterance)
+    } else {
+      console.error("API SpeechSynthesis no disponible en este navegador.")
+    }
   }
 
   return (
-    <div style={{ height: '70vh', width: '50%' }}>
+    <div style={{ height: '100vh', width: '100%' }}>
       <ReactReader
         url={url}
         location={location}
         locationChanged={handleLocationChanged}
+        getRendition={(rendition) => {
+          renditionRef.current = rendition
+        }}
       />
+      <div className='btn-group'>
+        <button onClick={narrateCurrentPage}>
+          Narrar Página
+        </button>
+        <button onClick={() => { window.speechSynthesis.cancel() }}>
+          Detener
+        </button>
+        <button onClick={() => { window.speechSynthesis.pause() }}>
+          Pausar
+        </button>
+        <button onClick={() => { window.speechSynthesis.resume() }}>
+          Continuar
+        </button>
+      </div>
     </div>
   )
 }
