6e3568e5a35f8e51866c9ad0a0203071b52006b9
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-08 01:59:13 -0400

    buscador interfaz

--- frontend/src/layouts/LayoutContent.jsx
+++ frontend/src/layouts/LayoutContent.jsx
@@ -4,25 +4,21 @@ import { Link } from 'react-router-dom'
 import '@styles/layout.css'
 
 
-export const LayoutContent = ({ title, path }) => {
-  const [content, error] = useGet(`http://localhost:4000/api/book?section=${path}`)
+export const LayoutContent = ({ section, search }) => {
+  const [content, error] = useGet(`http://localhost:4000/api/book?section=${section}&search=${search}`)
 
   if (error)
     return <h3 className='text-danger'>Surgio un Problema</h3>
 
   return (
-    <div className='layoutContent-container'>
-      <h2 className='h1 fw-bold pt-2 pb-5'>{title}</h2>
-
-      <section className='row g-5'>
-        {!!content && content.map(({ id, url_portada, nombre, autor }) => {
-          return (
-            <Link to={`/book/${id}`} key={id} className='col-12 col-md-6 col-lg-3'>
-              <CardBook url_image={url_portada} title={nombre} author={autor} />
-            </Link>
-          )
-        })}
-      </section>
-    </div>
+    <section className='row g-5 layoutContent-container'>
+      {!!content && content.map(({ id, url_portada, nombre, autor }) => {
+        return (
+          <Link to={`/book/${id}`} key={id} className='col-12 col-md-6 col-lg-3'>
+            <CardBook url_image={url_portada} title={nombre} author={autor} />
+          </Link>
+        )
+      })}
+    </section>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,34 @@
 import { NavbarSections } from '@components/NavbarSections'
 import { LayoutContent } from '@layouts/LayoutContent'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 
 const sections = [
-  {
-    id: 'btn-section-sugerencias',
-    label: 'Sugerencias'
-  },
-  {
-    id: 'btn-section-nuevos_lanzamientos',
-    label: 'Nuevos Lanzamientos'
-  },
-  {
-    id: 'btn-section-populares',
-    label: 'Populares'
-  },
-  {
-    id: 'btn-section-proximos_lanzamientos',
-    label: 'Próximos Lanzamientos'
-  },
+  { id: 'btn-section-sugerencias', label: 'Sugerencias' },
+  { id: 'btn-section-nuevos_lanzamientos', label: 'Nuevos Lanzamientos' },
+  { id: 'btn-section-populares', label: 'Populares' },
+  { id: 'btn-section-proximos_lanzamientos', label: 'Próximos Lanzamientos' },
 ]
 
 const MainPage = () => {
   const [currentSection, setCurrentSection] = useState(0)
+  const [onSearch, setOnSearch] = useState(false)
+  const [question, setQuestion] = useState('')
+  const location = useLocation()
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search)
+    const searchQuery = params.get('search')
+
+    if (searchQuery) {
+      setOnSearch(true)
+      setQuestion(searchQuery)
+    } else {
+      setOnSearch(false)
+      setQuestion('')
+    }
+  }, [location.search])
 
   const toggleSection = (idx) => {
     setCurrentSection(idx)
@@ -31,16 +36,31 @@ const MainPage = () => {
 
   return (
     <div className='container'>
-      <NavbarSections
-        currentSection={currentSection}
-        toggleSection={toggleSection}
-        sections={sections}
-      />
+      {onSearch ? (
+        <div className='mt-5 mb-5'>
+          <h2 className='pb-5'>
+            <span className='h1 fw-bold'>Resultados: {question}</span>
+          </h2>
 
-      <LayoutContent
-        title={sections[currentSection].label}
-        path={sections[currentSection].label.split('-').pop()}
-      />
+          <LayoutContent search={question} />
+        </div>
+      ) : (
+        <div className='pb-5'>
+          <NavbarSections
+            currentSection={currentSection}
+            toggleSection={toggleSection}
+            sections={sections}
+          />
+
+          <section>
+            <h2 className='h1 fw-bold pt-2 pb-5'>{sections[currentSection].label}</h2>
+
+            <LayoutContent
+              section={sections[currentSection].label.split('-').pop()}
+            />
+          </section>
+        </div>
+      )}
     </div>
   )
 }
