090403fc338f8e1c3a23631290e91928c10d05d6
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-11 07:49:51 -0400

    Update init.sql

--- db/structure/init.sql
+++ db/structure/init.sql
@@ -45,6 +45,7 @@ CREATE TABLE R_ESTRENO_CATEGORIA (
 );
 
 CREATE TABLE VISUALIZACION (
+    id SERIAL PRIMARY KEY,
     id_contenido INT NOT NULL,
     fecha DATE DEFAULT NOW(),
     FOREIGN KEY (id_contenido) REFERENCES CONTENIDO(id)
@@ -55,35 +56,35 @@ CREATE TABLE VISUALIZACION (
 -- FUNCTIONS
 CREATE OR REPLACE FUNCTION buscar(pattern TEXT)
 RETURNS TABLE(
-  id INT,
-  nombre VARCHAR,
-  autor VARCHAR,
-  url_texto VARCHAR,
-  url_portada VARCHAR,
-  url_audio VARCHAR,
-  categorias JSON
+    id INT,
+    nombre VARCHAR,
+    autor VARCHAR,
+    url_texto VARCHAR,
+    url_portada VARCHAR,
+    url_audio VARCHAR,
+    categorias JSON
 ) AS $$
 BEGIN
     RETURN QUERY
     SELECT
-      co.id,
-      co.nombre,
-      co.autor,
-      co.url_texto,
-      co.url_portada,
-      co.url_audio,
-      COALESCE(
-        json_agg(
-          json_build_object(
-            'id', ca.id,
-            'name', ca.nombre
-          )
-        ) FILTER (WHERE ca.id IS NOT NULL),
-        '[]'
-      ) AS categorias
+        co.id,
+        co.nombre,
+        co.autor,
+        co.url_texto,
+        co.url_portada,
+        co.url_audio,
+        COALESCE(
+            json_agg(
+            json_build_object(
+                'id', ca.id,
+                'name', ca.nombre
+            )
+            ) FILTER (WHERE ca.id IS NOT NULL),
+            '[]'
+        ) AS categorias
     FROM CONTENIDO co
-      LEFT JOIN R_CONTENIDO_CATEGORIA r ON id_contenido = co.id
-      LEFT JOIN CATEGORIA ca ON ca.id = id_categoria
+        LEFT JOIN R_CONTENIDO_CATEGORIA r ON id_contenido = co.id
+        LEFT JOIN CATEGORIA ca ON ca.id = id_categoria
     WHERE co.nombre ~* pattern OR co.autor ~* pattern
     GROUP BY co.id, co.nombre, co.autor, co.url_texto, co.url_portada, co.url_audio;
 END;
@@ -93,6 +94,24 @@ $$ LANGUAGE plpgsql;
 
 -- DATA
 INSERT INTO CONTENIDO (nombre, autor, url_texto, url_portada, url_audio) VALUES
-    ('Vacunas Verdades mentiras y controversia', 'Peter C Gotzsche', 'http://localhost:4000/uploads/epub/vacunas-verdades-mentiras-y-controversia-int.epub', 'http://localhost:4000/uploads/imgs/vacunas-verdades-mentiras-y-controversia-int.jpg', 'http://localhost:4000/uploads/tracks/vacunas-verdades-mentiras-y-controversia-int.mp3'),
-    ('¡Es la microbiota, idiota!', 'Sari Arponen', 'http://localhost:4000/uploads/epub/Es_la_microbiota,_idiota!_Alienta_Descubre_de_Sari_Arponen_Vista.epub', 'http://localhost:4000/uploads/imgs/Es_la_microbiota,_idiota!_Alienta_Descubre_de_Sari_Arponen_Vista.jpeg', 'http://localhost:4000/uploads/tracks/Es_la_microbiota,_idiota!_Alienta_Descubre_de_Sari_Arponen_Vista.mp3'),
-    ('Dime qué comes y te diré qué bacterias tienes', 'Blanca García-Orea Haro', 'http://localhost:4000/uploads/epub/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.epub', 'http://localhost:4000/uploads/imgs/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.jpg', 'http://localhost:4000/uploads/tracks/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.mp3');
+    ('Dime qué comes y te diré qué bacterias tienes', 'Blanca García-Orea Haro', 'http://localhost:4000/uploads/epub/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.epub', 'http://localhost:4000/uploads/imgs/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.jpg', 'http://localhost:4000/uploads/tracks/Dime_qué_comes_y_te_diré_qué_bacterias_tienes_Audiolibro_de_Blanca.mp3'),
+    ('Diferencias entre virus, bacterias y hongos', 'Dr. Elena R. Martínez', 'http://localhost:4000/uploads/epub/bacteriasVirus.epub', 'http://localhost:4000/uploads/imgs/bacteriasVirus.jpg', 'http://localhost:4000/uploads/tracks/bacteriasVirus.mp3'),
+    ('El cancer no es una enfermedad', 'Dr. Andreas Morizt', 'http://localhost:4000/uploads/epub/cancer.epub', 'http://localhost:4000/uploads/imgs/cancer.jpg', 'http://localhost:4000/uploads/tracks/cancer.mp3'),
+    ('¿La marihuana es mala para el cerebro?', 'Mikel Alonso', 'http://localhost:4000/uploads/epub/marihuana.epub', 'http://localhost:4000/uploads/imgs/marihuana.jpg', 'http://localhost:4000/uploads/tracks/marihuana.mp3'),
+    ('Pandemia, Epidemia y Endemia', 'Dr. Ignacio Lopez -Goñi', 'http://localhost:4000/uploads/epub/virusPandemias.epub', 'http://localhost:4000/uploads/imgs/virusPandemias.jpg', 'http://localhost:4000/uploads/tracks/virusPandemias.mp3');
+
+INSERT INTO ESTRENO (nombre, autor, url_portada) VALUES
+    ('Vacunas Verdades mentiras y controversia', 'Peter C Gotzsche', 'http://localhost:4000/uploads/imgs/vacunas-verdades-mentiras-y-controversia-int.jpg'),
+    ('¡Es la microbiota, idiota!', 'Sari Arponen', 'http://localhost:4000/uploads/imgs/Es_la_microbiota,_idiota!_Alienta_Descubre_de_Sari_Arponen_Vista.jpeg');
+
+INSERT INTO VISUALIZACION (id_contenido, fecha)
+SELECT
+    c.id AS id_contenido,
+    (CURRENT_DATE - INTERVAL '70 days' * RANDOM())::DATE AS fecha
+FROM CONTENIDO c,
+    GENERATE_SERIES(1, (FLOOR(RANDOM() * 100 + 1))::INT) AS s;
+
+UPDATE CONTENIDO
+SET fecha_subida = (
+    DATE '2024-08-01' + (RANDOM() * (DATE '2024-10-10' - DATE '2024-08-01'))::INT
+);
