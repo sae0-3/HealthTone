b49e73562d38dffc53bef9e2048e273b34851a6f
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-13 09:35:58 -0400

    fuente ajustada

--- frontend/src/components/EpubViewer.jsx
+++ frontend/src/components/EpubViewer.jsx
@@ -6,17 +6,21 @@ export const EpubViewer = ({ url }) => {
   const [location, setLocation] = useState(null)
   const renditionRef = useRef(null)
 
-  const handleLocationChanged = (newLocation) => {
-    setLocation(newLocation)
-  }
-
   return (
     <ReactReader
       url={url}
       location={location}
-      locationChanged={handleLocationChanged}
+      locationChanged={(newLocation) => { setLocation(newLocation) }}
       getRendition={(rendition) => {
         renditionRef.current = rendition
+        renditionRef.current.themes.register('customFont', {
+          body: {
+            'font-size': '1.1rem',
+            'font-family': 'Arial, san-serif',
+            'line-height': '1.5rem'
+          }
+        })
+        rendition.themes.select('customFont')
       }}
     />
   )
