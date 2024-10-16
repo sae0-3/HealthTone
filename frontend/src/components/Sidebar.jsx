3e69dea1f68364b48fea0cd64a9343ede66df4d0
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-16 19:23:26 -0400

    Sidebar actualizado

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 
 export const Sidebar = ({ toggleSidebar }) => {
-  const location = useLocation()
+  const { pathname, search } = useLocation()
   const options = [
     { label: 'Para ti', icon: 'house', to: '/' },
     { label: 'Explorar', icon: 'search', to: '/explore' },
@@ -13,7 +12,7 @@ export const Sidebar = ({ toggleSidebar }) => {
     <aside className='bg-htc-white h-screen w-full lg:w-52'>
       <div className='h-20 flex justify-between items-center px-3 lg:justify-center'>
         <Link to='/'>
-          <img src='/healthtone_black.svg' className={`w-16 ${location.pathname === '/'?'from-htc-lightblue':'from-gray'}`} alt='logo-healthtone' />
+          <img src='/healthtone_black.svg' className='w-16' alt='logo-healthtone' />
         </Link>
         <button type='button' onClick={() => { toggleSidebar() }}>
           <i className='bi bi-x text-5xl lg:hidden'></i>
@@ -24,7 +23,7 @@ export const Sidebar = ({ toggleSidebar }) => {
         {options.map(({ label, icon, to }) => (
           <Link to={to}
             key={to}
-            className={`flex gap-4 items-center justify-center bg-gradient-to-r ${location.pathname === to?'from-htc-lightblue':'from-gray'} to-white-500 py-3`}
+            className={`flex gap-4 items-center justify-center bg-gradient-to-r ${pathname == to && !search && 'from-htc-lightblue'}  to-white-500 py-3`}
             onClick={() => { toggleSidebar() }}
           >
             <i className={`bi bi-${icon} text-lg`}></i>
