bd89dc99be928a0c99e31f15db0e1931be2fd5e1
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-27 02:19:02 -0400

    links de recursos

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,23 +1,15 @@
 import cors from 'cors'
 import exprees from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import bookRouter from './routes.js'
 
 
 const app = exprees()
 const PORT = 4000
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 app.use(cors())
 app.use(exprees.json())
 
 app.use('/api/book', bookRouter)
-
-app.use('/uploads', exprees.static(path.join(__dirname, 'uploads')))
-
 
 app.listen(PORT, () => {
   console.log(`El servidor se esta ejecutando en el puerto ${PORT}`)
