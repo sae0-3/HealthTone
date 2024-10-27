3650953b4989c7fdd4721cb7a6cc7f598f1b9243
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-27 13:19:59 -0400

    endpoint register

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,8 @@
 import cors from 'cors'
 import exprees from 'express'
+import session from 'express-session'
+import passport from './config/passport.js'
+import authRoutes from './routes/auth.routes.js'
 import bookRoutes from './routes/book.routes.js'
 
 
@@ -9,6 +12,16 @@ const PORT = 4000
 app.use(cors())
 app.use(exprees.json())
 
+app.use(session({
+  secret: 'key-healthtone-v1',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api/auth', authRoutes)
 app.use('/api/book', bookRoutes)
 
 app.listen(PORT, () => {
