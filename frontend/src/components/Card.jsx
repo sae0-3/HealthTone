cb870c0ff2578d1dd18370a4fce96faef9a03095
jhoestar <202202883@est.umss.edu>
2024-10-29 02:13:18 -0400

    sincronizacion de cards

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,35 +1,35 @@
 import { useStore } from '@/hooks/useStore'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Card = ({ id, title, author, url_cover, url_audio, categories, disabled }) => {
-  const { setPlaying, setCurrentAudio, startAudio } = useStore()
-  const [isFavorite, setIsFavorite] = useState(false) // Estado para el botón de favoritos
-  const [iconPlay, setIconPlay] = useState(false)
+  const { setPlaying, setCurrentAudio, startAudio, currentAudio } = useStore()
+  const [isFavorite, setIsFavorite] = useState(false)
   const [iconHeart, setIconHeart] = useState(false)
+  const isPlaying = currentAudio?.id === id
 
   const handlePlay = (e) => {
     e.preventDefault()
     e.stopPropagation()
+    
     if (!disabled) {
+      if (!isPlaying) setPlaying(true)
+
       setCurrentAudio({ id, title, author, cover: url_cover, url: url_audio })
       startAudio()
-      setPlaying(true)
     }
   }
 
   const handleFavorite = (e) => {
     e.preventDefault()
-    e.stopPropagation() // Evita que el clic en el botón active handlePlay
-    setIsFavorite(isFavorite => !isFavorite) // Alterna el estado de favorito
-    setIconHeart(iconHeart => !iconHeart)
+    e.stopPropagation()
+    setIsFavorite(!isFavorite)
+    setIconHeart(!iconHeart)
   }
 
   return (
     <Link to={`/book/${id}`}>
-      <div
-        className={`group ${disabled && 'cursor-auto'} relative group`}
-      >
+      <div className={`group ${disabled && 'cursor-auto'} relative group`}>
         <div className='aspect-square overflow-hidden relative'>
           <img
             className={`m-auto h-full ${!disabled && 'group-hover:scale-105 group-focus:scale-105 transition-transform duration-500'}`}
@@ -40,26 +40,26 @@ export const Card = ({ id, title, author, url_cover, url_audio, categories, disa
             <button
               onClick={handlePlay}
               className={`absolute bottom-2 left-2 rounded-md p-2 transition-colors duration-300 hidden group-hover:block
-                          ${iconPlay ? 'bg-htc-white text-black' : 'bg-htc-lightblue text-white'}
+                          ${isPlaying ? 'bg-htc-lightblue text-white' : 'bg-htc-white text-black'}
               `}
               style={{ width: '40px', height: '40px' }}
             >
               <div className="flex items-center justify-center h-full">
-                <i className={iconPlay ? `bi bi-play text-2xl` : `bi bi-play-fill text-2xl`}></i>
+                <i className={isPlaying ? `bi bi-play-fill text-2xl` : `bi bi-play text-2xl`}></i>
               </div>
             </button>
           )}
 
-          {/* Botón de Favoritos en forma de corazón */}
-          {!disabled && <button
-            onClick={handleFavorite}
-            className={`absolute bottom-2 right-2 rounded-md p-2 transition-colors duration-300 hidden group-hover:block
-              ${iconHeart ? 'bg-htc-white text-black ' : 'bg-htc-lightblue text-white'
-              }`}
-            style={{ width: '40px', height: '40px' }}
-          >
-            <i className={iconHeart ? `bi bi-heart text-lg` : `bi bi-heart-fill text-lg`}></i>
-          </button>}
+          {!disabled && (
+            <button
+              onClick={handleFavorite}
+              className={`absolute bottom-2 right-2 rounded-md p-2 transition-colors duration-300 hidden group-hover:block
+                ${iconHeart ? 'bg-htc-lightblue text-white' : 'bg-htc-white text-black'}`}
+              style={{ width: '40px', height: '40px' }}
+            >
+              <i className={iconHeart ? `bi bi-heart-fill text-lg` : `bi bi-heart text-lg`}></i>
+            </button>
+          )}
         </div>
 
         <div className='px-3 py-4'>
@@ -70,7 +70,8 @@ export const Card = ({ id, title, author, url_cover, url_audio, categories, disa
         {!!categories && categories.length > 0 && (
           <div className='px-6 pt-4 pb-2'>
             {categories.map(({ nombre }, idx) => (
-              <span key={`${nombre}-${idx}`}
+              <span
+                key={`${nombre}-${idx}`}
                 className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
               >
                 #{nombre}
@@ -79,6 +80,6 @@ export const Card = ({ id, title, author, url_cover, url_audio, categories, disa
           </div>
         )}
       </div>
-     </Link>
+    </Link>
   )
 }
