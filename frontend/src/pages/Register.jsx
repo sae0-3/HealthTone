551c6c532ca8c5d8cd0d8fdb11afbd4e376122f3
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-29 02:10:19 -0400

    funcionalidad base al registro

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,117 +1,114 @@
+import axios from 'axios'
 import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 export const Register = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const [error, setError] = useState('')
+  const [error, setError] = useState(null)
+  const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    if (!name || !email || !password || !confirmPassword) {
-      setError('Todos los campos son obligatorios')
-      return
+    try {
+      await axios.post('http://localhost:4000/api/auth/register', { name, email, password })
+      setError(null)
+      navigate('/')
+    } catch (err) {
+      setError(err.response.data.error)
     }
-
-    if (password !== confirmPassword) {
-      setError('Las contraseñas no coinciden')
-      return
-    }
-
-    //**********lógica para registrar al usuario
-    console.log('Usuario registrado:', { name, email, password })
-
-    setName('')
-    setEmail('')
-    setPassword('')
-    setConfirmPassword('')
-    setError('')
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-2">
-      <div className="grid gap-2 max-w-2xl w-full mx-auto">
-        <div className="flex justify-center">
-          <img src="/healthtone_black.svg" className="w-16" alt="logo-healthtone" />
+    <div className='flex items-center justify-center min-h-screen bg-gray-100 px-2 py-5'>
+      <div className='grid gap-2 max-w-2xl w-full mx-auto'>
+        <div className='flex justify-center'>
+          <img src='/healthtone_black.svg' className='w-16' alt='logo-healthtone' />
         </div>
 
-        <div className="text-4xl text-center">
+        <div className='text-4xl text-center'>
           <h1>
             Bienvenido a <br />
-            <span className="text-htc-lightblue">Healthtone</span>
+            <span className='text-htc-lightblue'>Healthtone</span>
           </h1>
         </div>
 
-        <div className="flex justify-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-            <h2 className="text-2xl font-bold text-center mb-6">Registro de Usuarios</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className='flex justify-center'>
+          <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md'>
+            <h2 className='text-2xl font-bold text-center mb-6'>Registro de Usuarios</h2>
+            <form className='space-y-6' onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
                   Nombre
                 </label>
                 <input
-                  id="name"
-                  type="text"
+                  id='name'
+                  type='text'
                   required
-                  className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Ingrese su nombre"
+                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  placeholder='Ingrese su nombre'
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
 
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
                   Correo Electrónico
                 </label>
                 <input
-                  id="email"
-                  type="email"
+                  id='email'
+                  type='email'
                   required
-                  className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Ingrese su correo"
+                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  placeholder='Ingrese su correo'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
 
               <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
                   Contraseña
                 </label>
                 <input
-                  id="password"
-                  type="password"
+                  id='password'
+                  type='password'
                   required
-                  className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Ingrese su contraseña"
+                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  placeholder='Ingrese su contraseña'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
+                {error && <p style={{ color: 'red' }}>{error}</p>}
               </div>
 
               <div>
-                <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+                <label htmlFor='confirmPassword' className='block text-sm font-medium text-gray-700'>
                   Confirmar Contraseña
                 </label>
                 <input
-                  id="confirmPassword"
-                  type="password"
+                  id='confirmPassword'
+                  type='password'
                   required
-                  className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Confirme su contraseña"
+                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  placeholder='Confirme su contraseña'
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                 />
+                {password !== confirmPassword && (
+                  <p className='text-red-600'>La contraseña no coincide</p>
+                )}
               </div>
 
               <button
-                type="submit"
-                className="w-full py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-htc-lightblue hover:bg-htc-blue focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                type='submit'
+                className='w-full py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-htc-lightblue hover:bg-htc-blue focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-40'
+                disabled={password !== confirmPassword}
               >
                 Registrarse
               </button>
@@ -119,11 +116,14 @@ export const Register = () => {
           </div>
         </div>
 
-        <div className="text-sm text-center">
+        <div className='text-sm text-center'>
           <span>¿Ya tienes una cuenta? Inicia sesión </span>
-          <a href="/login" className="font-medium text-htc-lightblue hover:text-htc-blue border-b border-htc-blue hover:border-htc-blue">
+          <Link
+            to='/login'
+            className='font-medium text-htc-lightblue hover:text-htc-blue border-b border-htc-blue hover:border-htc-blue'
+          >
             aquí
-          </a>
+          </Link>
         </div>
       </div>
     </div>
