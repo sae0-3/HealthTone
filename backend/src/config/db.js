523a3a3c1dd9514e44c30a462f19ae6525e04ddd
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-10-29 05:56:46 -0400

    base de datos en la nube

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,11 +4,16 @@ import pg from 'pg'
 const { Pool } = pg
 
 export const pool = new Pool({
-  user: 'healthtone',
-  password: '1234',
-  host: 'localhost',
-  database: 'db_healthtone',
-  port: 5432
+  // user: 'healthtone',
+  // password: '1234',
+  // host: 'localhost',
+  // database: 'db_healthtone',
+  // port: 5432,
+  user: 'postgres.ehtxpvdysxarrsjrnxxx',
+  password: 'contraseña-HEALTHTONE',
+  host: 'aws-0-us-west-1.pooler.supabase.com',
+  database: 'postgres',
+  port: 6543,
 })
 
 async function conectarDB(reintentos = 5, espera = 2000) {
