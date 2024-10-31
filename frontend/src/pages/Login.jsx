44295251a3ea8517ebe40e6bb346bf95013f5194
jhoestar <202202883@est.umss.edu>
2024-10-31 13:12:32 -0400

    bug eye

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -71,22 +71,26 @@ export const Login = () => {
                 <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
                   Contraseña
                 </label>
-                <input
-                  id='password'
-                  type={showPassword ? 'text' : 'password'}
-                  name='password'
-                  required
-                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-                  placeholder='Ingrese su contraseña'
-                  onChange={handleChange}
-                />
-                <button
-                  type='button'
-                  className='absolute inset-y-0 right-0 flex items-center pt-6 pr-3'
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  <i className={`bi bi-eye${showPassword ? '-fill' : ''}`}></i>
-                </button>
+
+                <div className='relative'>
+                  <input
+                    id='password'
+                    type={showPassword ? 'text' : 'password'}
+                    name='password'
+                    required
+                    className='mt-1 w-full px-3 py-2 pr-10 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                    placeholder='Ingrese su contraseña'
+                    onChange={handleChange}
+                  />
+                  <button
+                    type='button'
+                    className='absolute right-3 top-[calc(50%+1px)] transform -translate-y-1/2 flex items-center'
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    <i className={`bi bi-eye${showPassword ? '-fill' : ''}`}></i>
+                  </button>
+                </div>
+
                 {error && error.status === 401 && (
                   <p className='text-red-600'>Contraseña incorrecta</p>
                 )}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -84,22 +84,25 @@ export const Register = () => {
                 <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
                   Contraseña
                 </label>
-                <input
-                  id='password'
-                  type={showPassword ? 'text' : 'password'}
-                  name='password'
-                  required
-                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-                  placeholder='Ingrese su contraseña'
-                  onChange={handleChange}
-                />
-                <button
-                  type='button'
-                  className='absolute inset-y-0 right-0 flex items-center pt-6 pr-3'
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  <i className={`bi bi-eye${showPassword ? '-fill' : ''}`}></i>
-                </button>
+
+                <div className='relative'>
+                  <input
+                    id='password'
+                    type={showPassword ? 'text' : 'password'}
+                    name='password'
+                    required
+                    className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                    placeholder='Ingrese su contraseña'
+                    onChange={handleChange}
+                  />
+                  <button
+                    type='button'
+                    className='absolute right-3 top-[calc(50%+1px)] transform -translate-y-1/2 flex items-center'
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    <i className={`bi bi-eye${showPassword ? '-fill' : ''}`}></i>
+                  </button>
+                </div>
 
                 {error && (
                   <p className='text-red-600'>{error?.response?.data?.error}</p>
@@ -110,22 +113,25 @@ export const Register = () => {
                 <label htmlFor='confirmPassword' className='block text-sm font-medium text-gray-700'>
                   Confirmar Contraseña
                 </label>
-                <input
-                  id='confirmPassword'
-                  type={showConfirmPassword ? 'text' : 'password'}
-                  required
-                  name='confirmPassword'
-                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-                  placeholder='Confirme su contraseña'
-                  onChange={handleChange}
-                />
-                <button
-                  type='button'
-                  className='absolute inset-y-0 right-0 flex items-center pt-6 pr-3'
-                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                >
-                  <i className={`bi bi-eye${showConfirmPassword ? '-fill' : ''}`}></i>
-                </button>
+
+                <div className='relative'>
+                  <input
+                    id='confirmPassword'
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    required
+                    name='confirmPassword'
+                    className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                    placeholder='Confirme su contraseña'
+                    onChange={handleChange}
+                  />
+                  <button
+                    type='button'
+                    className='absolute right-3 top-[calc(50%+1px)] transform -translate-y-1/2 flex items-center'
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  >
+                    <i className={`bi bi-eye${showConfirmPassword ? '-fill' : ''}`}></i>
+                  </button>
+                </div>
 
                 {formData.password !== formData.confirmPassword && (
                   <p className='text-red-600'>La contraseña no coincide</p>
