52037db71a3c49456f36f6aaf9ae829593d93d2d
jhoestar <202202883@est.umss.edu>
2024-11-06 02:32:37 -0400

    component email

--- frontend/src/pages/RecoveryPassword.jsx
+++ frontend/src/pages/RecoveryPassword.jsx
@@ -1,17 +1,9 @@
 
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import RecoverEmail from '../components/RecoverEmail'
 
 
 const RecoveryPassword = () => {
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setFormData({ ...formData, [name]: value })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    login(formData)
-  }
 
 
   return (
@@ -27,36 +19,7 @@ const RecoveryPassword = () => {
             <span className='text-htc-lightblue'>Healthtone</span>
           </h1>
         </div>
-
-        <div className='flex justify-center'>
-          <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md'>
-            <h2 className='text-2xl font-bold text-center mb-6'>Recuperar Contraseña</h2>
-            <form className='space-y-6' onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
-                  Ingrese el correo electronico de la cuenta que desea recuperar
-                </label>
-                <input
-                  id='email'
-                  type='email'
-                  name='email'
-                  required
-                  className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-                  placeholder='Ingrese su correo'
-                  onChange={handleChange}
-                />
-              </div>
-
-              <button
-                type='submit'
-                className='w-full py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-htc-lightblue hover:bg-htc-blue focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50'
-              >
-                Recuperar Contraseña
-              </button>
-            </form>
-          </div>
-        </div>
-
+          <RecoverEmail />
         <div className='text-sm text-center'>
           <span>¿No tienes una cuenta? Crea una </span>
           <Link to='/register' className='font-medium text-htc-lightblue hover:text-htc-blue border-b border-htc-blue hover:border-htc-blue'>
