83a3bc9084266d68ff5528d74958d23a5230f04a
jhoestar <202202883@est.umss.edu>
2024-11-06 03:59:51 -0400

    Email and recoveryContext

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useContext } from 'react'
+import { RecoveryContext } from '../pages/RecoveryPassword'
 
-const Email = ({handleSubmit}) => {
+const Email = () => {
 
-  const [data, setData] = useState('')
-
-  const handleChange = (e) => {
-    const { value } = e.target
-    setData({ ...data, value })
+  const {setEmail, email, setPage} = useContext(RecoveryContext)
+  
+  const handleChange = (e) => { 
+    setEmail(e.target.value)
   }
 
-  const handleFormSubmit = (e) => {
+  const handleSubmit = (e) => {
+    console.log(email)
     e.preventDefault()
-    handleSubmit(e, data)
   }
 
   return (
     <div className='flex justify-center'>
       <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md'>
         <h2 className='text-2xl font-bold text-center mb-6'>Recuperar Contraseña</h2>
-        <form className='space-y-6' onSubmit={handleFormSubmit}>
+        <form className='space-y-6' onSubmit={handleSubmit}>
           <div>
             <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
               Ingrese el correo electronico de la cuenta que desea recuperar
@@ -28,7 +27,7 @@ const Email = ({handleSubmit}) => {
               id='email'
               type='email'
               name='email'
-              value={data}
+              value={email}
               required
               className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
               placeholder='Ingrese su correo'
--- frontend/src/pages/RecoveryPassword.jsx
+++ frontend/src/pages/RecoveryPassword.jsx
@@ -1,39 +1,46 @@
 
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Email from '../components/Email'
-import { useContext } from 'react'
+import { createContext, useContext, useState} from 'react'
+
+export const RecoveryContext = createContext()
 
 const RecoveryPassword = () => {
 
-
-  const handleSubmitEmail = async (e, formData) => {
-    e.preventDefault()
-    //sendCodeOTP(formData)
-  }
-
+  const [page, setPage] = useState('Email')
+  const [email, setEmail] = useState('')
+  const [otp, setOtp] = useState('')
 
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-100 px-2 py-5'>
-      <div className='grid gap-2 max-w-2xl w-full mx-auto'>
-        <div className='flex justify-center'>
-          <img src='/healthtone_black.svg' className='w-24' alt='logo-healthtone' />
-        </div>
+    <RecoveryContext.Provider
+      value={{ page, setPage, otp, setOtp, email, setEmail }}
+    >
+      <div className='flex items-center justify-center min-h-screen bg-gray-100 px-2 py-5'>
+        <div className='grid gap-2 max-w-2xl w-full mx-auto'>
+          <div className='flex justify-center'>
+            <img src='/healthtone_black.svg' className='w-24' alt='logo-healthtone' />
+          </div>
 
-        <div className='text-4xl text-center'>
-          <h1>
-            Bienvenido a <br />
-            <span className='text-htc-lightblue'>Healthtone</span>
-          </h1>
-        </div>
-          <Email onSubmit={handleSubmitEmail}/>
-        <div className='text-sm text-center'>
-          <span>¿No tienes una cuenta? Crea una </span>
-          <Link to='/register' className='font-medium text-htc-lightblue hover:text-htc-blue border-b border-htc-blue hover:border-htc-blue'>
-            aquí
-          </Link>
+          <div className='text-4xl text-center'>
+            <h1>
+              Bienvenido a <br />
+              <span className='text-htc-lightblue'>Healthtone</span>
+            </h1>
+          </div>
+
+          {page === 'Email' && <Email />}
+          {page === 'OTP' && <Email />}
+          {page === 'Recover' && <Email />}
+
+          <div className='text-sm text-center'>
+            <span>¿No tienes una cuenta? Crea una </span>
+            <Link to='/register' className='font-medium text-htc-lightblue hover:text-htc-blue border-b border-htc-blue hover:border-htc-blue'>
+              aquí
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </RecoveryContext.Provider>
   )
 }
 
