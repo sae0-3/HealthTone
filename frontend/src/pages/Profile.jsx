822d5ab704ac9c954148465a946f73ae0977e548
jhoestar <202202883@est.umss.edu>
2024-11-18 19:26:17 -0400

    select and date-picker

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,11 +3,85 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { Link } from 'react-router-dom'
 import PasswordUpdate from '../components/PasswordUpdate'
+import Select from 'react-select' // Importa el componente Select
 
 const Profile = () => {
   const [card, setCard] = useState('')
   const [isPasswordOpen, setIsPasswordOpen] = useState(false)
   const [isEditOpen, setisEditOpen] = useState(false)
+  const [date, setDate] = useState(Date())
+
+  const [data, setData] = useState({
+    correo: "",
+    fechaNacimiento: "",
+    pais: "",
+    numero: "",
+    genero: "Mujer",
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const [isFocused, setIsFocused] = useState({
+    birthDate: false,
+    country: false,
+    phone: false,
+    gender: false,
+    password: false,
+  })
+
+  const handleFocus = (field) => {
+    setIsFocused((prevState) => ({
+      ...prevState,
+      [field]: true,
+    }))
+  }
+
+  const handleBlur = (field) => {
+    setIsFocused((prevState) => ({
+      ...prevState,
+      [field]: false,
+    }))
+  }
+
+  const genderOptions = [
+    { value: 'Hombre', label: 'Hombre' },
+    { value: 'Mujer', label: 'Mujer' },
+  ]
+  
+  const countryOptions = [
+    { value: 'Bolivia', label: 'Bolivia' },
+    { value: 'Brasil', label: 'Brasil' },
+  ]
+
+  const style = {
+    control: (provided) => ({
+      ...provided,
+      borderColor: '#3E5C76',
+      borderWidth: '1px',
+      boxShadow: 'none',
+      '&:hover': {
+        borderColor: '#3E5C76',
+      },
+    }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 50,
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#3E5C76' : state.isFocused ? '#d9e2ec' : 'white',
+      color: state.isSelected ? 'white' : '#3E5C76',
+      '&:hover': {
+        backgroundColor: '#d9e2ec',
+      },
+    }),
+  }
 
   return (
     <div className="container mx-auto px-4">
@@ -19,8 +93,8 @@ const Profile = () => {
         ¡Apreciamos que seas un miembro de nuestra comunidad!
       </p>
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-4xl mx-auto flex flex-col gap-6">
-        <div className="flex flex-col items-center md:flex-row md:items-center md:justify-center gap-6">
-          <div className="w-28 h-28 md:w-32 md:h-32 rounded-full overflow-hidden border-4 border-htc-blue">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-start gap-6">
+          <div className="md:text-left w-28 h-28 md:w-32 md:h-32 rounded-full overflow-hidden border-2 border-htc-blue">
             <img
               src="src/assets/med.jpg"
               alt="User Avatar"
@@ -32,28 +106,63 @@ const Profile = () => {
             <p className="text-gray-600">Correo: usuario@email.com</p>
           </div>
         </div>
+
         <div className="flex flex-col gap-4">
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-2">
             <span className="text-gray-600 font-medium">Contraseña:</span>
-            <span className="text-gray-800 font-semibold">********</span>
+            <div className="text-gray-800 w-64">
+              <p>passwords</p>
+            </div>
           </div>
-          <div className="flex justify-between items-center">
+
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-2">
             <span className="text-gray-600 font-medium">Fecha de Nacimiento:</span>
-            <span className="text-gray-800">16 de Noviembre, 2024</span>
+            <div className="contenedor w-64">
+              <DatePicker
+                selected={date}
+                onChange={(fecha) => setDate(fecha)}
+              />
+            </div>
           </div>
-          <div className="flex justify-between items-center">
+
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-2">
             <span className="text-gray-600 font-medium">País:</span>
-            <span className="text-gray-800">Bolivia</span>
+            <Select
+              name="genero"
+              value={countryOptions.find(option => option.value === data.pais) || null}
+              onChange={(selectedOption) => setData({ ...data, pais: selectedOption.value })}
+              options={countryOptions}
+              className='w-64'
+              styles={style}
+              />
           </div>
-          <div className="flex justify-between items-center">
-            <span className="text-gray-600 font-medium">Numero de teléfono:</span>
-            <span className="text-gray-800">78340433</span>
+
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-2">
+            <span className="text-gray-600 font-medium">Número de teléfono:</span>
+            <input
+              type="text"
+              name="numero"
+              value={data.numero || '78340433'}
+              onFocus={() => handleFocus('phone')}
+              onBlur={() => handleBlur('phone')}
+              onChange={handleChange}
+              className={`text-gray-800 w-64 ${isFocused.phone ? 'border-b-2 border-htc-blue' : ''} focus:outline-none`}
+            />
           </div>
-          <div className="flex justify-between items-center">
+
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-2">
             <span className="text-gray-600 font-medium">Género:</span>
-            <span className="text-gray-800">Hombre</span>
+            <Select
+              name="genero"
+              value={genderOptions.find(option => option.value === data.genero) || null}
+              onChange={(selectedOption) => setData({ ...data, genero: selectedOption.value })}
+              options={genderOptions}
+              className='w-64'
+              styles={style}
+              />
           </div>
         </div>
+
         <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-between">
           <button className="text-left my-auto font-medium text-htc-lightblue hover:text-htc-blue">
             <span className="border-b border-htc-blue hover:border-htc-blue">
@@ -61,14 +170,14 @@ const Profile = () => {
             </span>
           </button>
           <div className="flex gap-2">
-            <button 
+            <button
               className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-htc-blue hover:text-white"
-              onClick={() => {setisEditOpen(true)}}>
+              onClick={() => { setisEditOpen(true) }} >
               Editar Perfil
             </button>
-            <button 
+            <button
               className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-htc-blue hover:text-white"
-              onClick={() => {setIsPasswordOpen(true)}}>
+              onClick={() => { setIsPasswordOpen(true) }} >
               Cambiar Contraseña
             </button>
           </div>
@@ -78,7 +187,7 @@ const Profile = () => {
       {isPasswordOpen && (
         <PasswordUpdate setOpen={setIsPasswordOpen} />
       )}
-      
+
       {isEditOpen && (
         <EditProfile setOpen={setisEditOpen} />
       )}
