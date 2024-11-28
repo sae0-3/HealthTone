ea709f21c2d8bedf956436439ffd51eeaaed5d6e
Darjom <danijorequeM@gmail.com>
2024-11-28 12:16:17 -0400

    fix bug sombreado

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -15,22 +15,25 @@ export const Categories = () => {
   }
 
   return (
-    <>
-      <h2 className='font-bold text-2xl mt-4'>Categorias</h2>
+<>
+  <h2 className='font-bold text-2xl mt-4'>Categorias</h2>
 
-      {categories && (
-        <div className='flex gap-1 mt-4 flex-wrap'>
-          {categories.map(({ id, name }) => (
-            <Link
-              key={id}
-              to={`/categories/${id}`}
-              className='flex gap-3 items-center bg-htc-lightblue rounded-md py-2 px-3 text-white'
-            >{name}</Link>
-          ))}
-        </div >
-      )}
+  {categories && (
+    <div className='flex gap-1 mt-4 flex-wrap'>
+      {categories.map(({ id, name }) => (
+        <Link
+          key={id}
+          to={`/categories/${id}`}
+          className='flex gap-3 items-center bg-htc-lightblue rounded-md py-2 px-3 text-white hover:bg-htc-darkblue hover:bg-opacity-80 transition duration-300'
+        >
+          {name}
+        </Link>
+      ))}
+    </div>
+  )}
 
-      <LayoutContent content={books} viewCategories />
-    </>
+  <LayoutContent content={books} viewCategories />
+</>
+
   )
 }
